e7aba97b2aa10312452f031a529182e08e4b2482
Camilly-Mart12 <camilly.martins1212@gmail.com>
2026-05-21 22:13:56 -0300

    Update filmecontroller.js

--- src/contoller/controllers/filmecontroller.js
+++ src/contoller/controllers/filmecontroller.js
@@ -1,11 +1,28 @@
 const FilmeModel = require("../models/FilmeModel")
 
-const Filmecontroller = {
+const FilmeController = {
     async getallFilmes (req, res) {
         const filmes = await FilmeModel.findAll()
 
         return response.json(filmes)
+    },
+    async createFilme( req, res ) {
+        const {
+            titulo,
+            diretorid,
+            genero,
+        }  = req.body
+
+        const filme = await FilmeModel.create(
+            {
+                titulo,
+                diretorid
+            },
+            generos
+        )
+        return res.status(201).json({ id: filmeId})
     }
+    
 }
 
 module.exports = FilmeController
